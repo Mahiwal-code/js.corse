18497389e9369251bbe3370fc4fe2e2f80fb1165
Mahiwal Sahu <96350589+Mahiwal-code@users.noreply.github.com>
2023-10-04 20:59:25 +0000

    some code

--- course/test.js
+++ course/test.js
@@ -1,1 +1,37 @@
-console.log("Mahiwal")
+//console.log("Mahiwal")
+for (let i = 0; i <= 10; i++) {
+    const element = i;
+    if (element == 5){
+       // console.log("5 is the number")
+    }
+   // console.log(element)
+    
+}
+for (let i = 1; i <= 10; i++) {
+   // console.log(`Table of: ${i}`);
+    for (let j = 1; j <= 10; j++) {
+        //console.log(`Inner loop value: ${i}`);
+       // console.log(`${i}*${j} = ${i*j}`)
+        
+        
+    }
+}
+
+//if (function x() {
+    //console.log(typeof x)
+//}){
+//console.log(typeof   x)}
+/*if(true){
+    function x(params) {
+        
+    }
+    console.log(typeof x)
+}
+function x(){}
+if (function x(){}) {
+    console.log(typeof x);
+}*/
+const a = 3
+console.log(typeof a)
+    
+    
